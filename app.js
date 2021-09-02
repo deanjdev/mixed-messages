b401e1d92e61515f0a92b5cf5f98e0cb25a38cd0
Anton2020 <koningcoding@gmail.com>
2021-09-02 23:40:29 +0200

    humble beginnings

---
+++ app.js
@@ -0,0 +1,9 @@
+//Lonely hearts random chat bot
+
+let sentenceList1 = ["Hello", "Hi", "Good morning"];
+let sentenceList2 = ["Gorgeous", "Handsome", "Smarty"];
+let sentenceList3 = [
+  "I missed you",
+  "Let's hang out some time",
+  "Did you think of me ?",
+];
